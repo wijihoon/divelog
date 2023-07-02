386818ddd6a9c952c6cc484feb5920189983c1a6
wijihoon123 <wijihoon123@naver.com>
2023-07-02 10:10:20 +0900

    02 SpringApplication 살펴보기

--- src/main/java/com/fastcampus/springrunner/divelog/DiveLogApplication.java
+++ src/main/java/com/fastcampus/springrunner/divelog/DiveLogApplication.java
@@ -1,13 +1,34 @@
 package com.fastcampus.springrunner.divelog;
 
+import org.springframework.boot.Banner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.context.ApplicationPidFileWriter;
+import org.springframework.boot.web.context.WebServerPortFileWriter;
 
 @SpringBootApplication
 public class DiveLogApplication {
 
 	public static void main(String[] args) {
+		for(String arg: args) {
+			System.out.println("Argument: " + arg);
+		}
+		System.out.println("Hello World!");
 		SpringApplication.run(DiveLogApplication.class, args);
-	}
 
+		// SpringApplicationBuilder 사용
+//		SpringApplication application = new SpringApplication(DiveLogApplication.class);
+//		application.setBannerMode(Banner.Mode.OFF);
+//		application.addListeners(new ApplicationPidFileWriter()
+//								, new WebServerPortFileWriter());
+//		application.run(args);
+
+		// SpringApplicationBuilder 사용2
+//		new SpringApplicationBuilder(DiveLogApplication.class)
+//				.bannerMode(Banner.Mode.OFF)
+//				.listeners(new ApplicationPidFileWriter()
+//						, new WebServerPortFileWriter())
+//				.build().run(args);
+	}
 }
--- src/test/java/com/fastcampus/springrunner/divelog/DiveLogApplicationTests.java
+++ src/test/java/com/fastcampus/springrunner/divelog/DiveLogApplicationTests.java
@@ -1,13 +1,19 @@
 package com.fastcampus.springrunner.divelog;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest(args={"--app.name=dive-log-test"})
 class DiveLogApplicationTests {
 
 	@Test
-	void contextLoads() {
+	void testApplicationArguments(@Autowired ApplicationArguments appArguments) {
+		assertThat(appArguments.getOptionNames()).contains("app.name");
+		assertThat(appArguments.getOptionValues("app.name")).contains("dive-log-test");
 	}
 
 }
